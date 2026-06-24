abc482b29a7c842f2c46eeaab5f9f7b5a791f554
Aadi.web3 <bt25cse115@iiitn.ac.in>
2026-06-24 19:31:24 +0000

    wrote Network_config from scratch

--- script/NetworkConfig.s.sol
+++ script/NetworkConfig.s.sol
@@ -3,9 +3,32 @@
 pragma solidity ^0.8.19;
 
 import {fundMe} from "../src/fundMe.sol";
-
+import {Script} from "forge-std/Script.sol";
 
 contract network_config{
+
+    address public final_addr;
+
+    constructor(){
+        if(block.chainid == 11155111){
+            final_addr = ret_sep_addr();
+        }
+        else{
+            final_addr = ret_anvil_addr();
+        }
+    }
+
+    struct chain_config {
+        address chain_address;
+    }
+
+    function ret_sep_addr() public view returns(chain_config memory){
+        chain_config memory addr_feed = chain_config{chain_address: 0x694AA1769357215DE4FAC081bf1f309aDC325306}();
+        return addr_feed;
+
+    }
+
+    function ret_anvil_addr() public view returns(chain_config memory){}
 
 }
 
